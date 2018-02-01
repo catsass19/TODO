4e476470dcbb79820864ba91eba5d28eac13bbf3
Chenwil <willy.chen@hpe.com>
2018-02-01 18:52:53 +0800

    fix text compositioning in TextField

--- src/components/insertTodo.tsx
+++ src/components/insertTodo.tsx
@@ -13,6 +13,8 @@ export interface InsertTodoState {
 
 export class InsertTodo extends React.Component<InsertTodoProps, InsertTodoState> {
 
+    private isCompositioning : boolean = false;
+
     constructor(props : InsertTodoProps) {
         super(props);
         this.state = {
@@ -20,15 +22,27 @@ export class InsertTodo extends React.Component<InsertTodoProps, InsertTodoState
         };
     }
     public render() {
+        /*
+            TypeScript pitfall.
+            Seems like composition events are not defined correctly in TextField
+            Even though props will be passed down to actual input element but TS compilation will still fail
+            In this case we wrap an input element inside of TextField
+         */
         return(
             <TextField
                 fullWidth={true}
                 floatingLabelText="Insert New TODO Item"
-                value={this.state.todoValue}
                 onChange={this.updateText}
                 onFocus={this.onFocus}
                 onBlur={this.onBlur}
-            />
+            >
+                <input
+                    value={this.state.todoValue}
+                    onCompositionStart={this.onComposition}
+                    onCompositionUpdate={this.onComposition}
+                    onCompositionEnd={this.endComposition}
+                />
+            </TextField>
         );
     }
     @autobind
@@ -46,10 +60,18 @@ export class InsertTodo extends React.Component<InsertTodoProps, InsertTodoState
     @autobind
     private kbHandler(event : KeyboardEvent) {
         if (event.key === "Enter") {
-            if (this.state.todoValue.length) {
+            if (!this.isCompositioning && this.state.todoValue.length) {
                 ToDoService.addTODO(this.state.todoValue);
                 this.setState({todoValue: ""});
             }
         }
+    }
+    @autobind
+    private onComposition() {
+        this.isCompositioning = true;
+    }
+    @autobind
+    private endComposition() {
+        this.isCompositioning = false;
     }
 }
